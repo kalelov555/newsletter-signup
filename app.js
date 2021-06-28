88dbf38f8feed918b239c968f6f394ebf8598031
kalelov555 <73077918+kalelov555@users.noreply.github.com>
2021-06-28 22:35:26 +0600

    Last upgrade

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.post("/", function(req, res) {
 
         response.on("data", function(data) {
 
-            console.log(JSON.parse(data));
+            console.log(data);
 
         })
     });
